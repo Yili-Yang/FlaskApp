600e4ec4fce6744046f1620c84f981a7c0041c2c
Yili Yang <yili.yang@columbia.edu>
2019-07-13 18:15:38 -0400

    add search bar

--- flaskapp.py
+++ flaskapp.py
@@ -4,26 +4,25 @@ import numpy as np
 app = Flask(__name__)
 
 
-@app.route("/")
-def test():
-    age = {"Sam": 20, "Ken": 30, "Grace": 23, "Peter": 29}
-    return render_template("test.html", ages=age)
-
-
 @app.route("/404")
 def page404():
     return render_template("404.html")
 
 
-@app.route("/dataframe")
+@app.route("/",methods=['GET','POST'])
 def display_data():
-    df = pd.read_csv('cleaned_asha_data.csv')
+    if request.method == 'POST':
+        df = pd.read_csv('cleaned_asha_data.csv')
+        df = df.sort_values('ReplyCount',ascending=False)
+        df.iloc[1,1]= request.form['text']
+    else:
+        df =pd.DataFrame(columns=['Name','PostTitle','PostDes','Date','Time','ReplyCount'])
     dataRow_L = list()
     for ind, item in df.iterrows():
         dataRow_L.append(item.tolist())
     col_list = df.columns.tolist()
-    return render_template("tables.html", cols=len(col_list), rows=len(df), dataRow_L=dataRow_L,columnName_L=col_list)
 
+    return render_template("index.html", cols=len(col_list), rows=len(df), dataRow_L=dataRow_L,columnName_L=col_list)
 
 if __name__ == "__main__":
 	app.run(debug =True)
