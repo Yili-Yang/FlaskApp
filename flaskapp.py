e920bdc332b67762895995e6921970570dc9583e
Yili Yang <yili.yang@columbia.edu>
2019-07-14 12:56:57 -0400

    updated datetime and search drop down

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import numpy as np
+import json
 app = Flask(__name__)
 
 
@@ -9,20 +10,30 @@ def page404():
     return render_template("404.html")
 
 
-@app.route("/",methods=['GET','POST'])
+@app.route("/", methods=['GET', 'POST'])
 def display_data():
     if request.method == 'POST':
         df = pd.read_csv('cleaned_asha_data.csv')
-        df = df.sort_values('ReplyCount',ascending=False)
-        df.iloc[1,1]= request.form['text']
+        df = df.sort_values('ReplyCount', ascending=False)
+        df.iloc[1, -1] = request.form['text']
+        df.iloc[2, -1] = request.form.get('startdate')
+        df.iloc[3, -1] = request.form.get('enddate')
+        with open('test.txt','w') as f:
+            json.dump(request.json, f)
     else:
-        df =pd.DataFrame(columns=['Name','PostTitle','PostDes','Date','Time','ReplyCount'])
+        df = pd.DataFrame(
+            columns=['Name', 'PostTitle', 'PostDes', 'Date', 'Time', 'ReplyCount'])
     dataRow_L = list()
     for ind, item in df.iterrows():
         dataRow_L.append(item.tolist())
     col_list = df.columns.tolist()
 
-    return render_template("index.html", cols=len(col_list), rows=len(df), dataRow_L=dataRow_L,columnName_L=col_list)
+    return render_template("index.html", cols=len(col_list), rows=len(df), dataRow_L=dataRow_L, columnName_L=col_list)
+
+@app.route("/test" , methods=['GET', 'POST'])
+def test():
+    select = request.form.get('languages')
+    return(str(select)) # just to see what select is
 
 if __name__ == "__main__":
-	app.run(debug =True)
+    app.run(debug=True)
---
+++ test bokeh.py
@@ -0,0 +1,10 @@
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html
+
+plot = figure()
+plot.circle([1,2], [3,4])
+
+html = file_html(plot, CDN, "my plot")
+with open('bokeh.txt','w') as f:
+	f.write(html)
